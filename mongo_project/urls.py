7c1094dc92c38074f2245ce03be4be652ebafb64
Daniel <dany.orop@gmail.com>
2019-04-22 18:47:21 -0400

    api 2 implemented

--- mongo_project/urls.py
+++ mongo_project/urls.py
@@ -1,18 +1,3 @@
-"""mongo_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from student import views
@@ -26,7 +11,9 @@ from student.views import (
     StudentListAPIView,
     StudentDetailAPIView,
     StudentUpdateAPIView,
-    StudentDeleteAPIView
+    StudentDeleteAPIView,
+    StudentListAPIView2,
+    StudentAPIView2,
 )
 
 urlpatterns = [
@@ -43,4 +30,7 @@ urlpatterns = [
     path('api/student/<int:pk>/', StudentDetailAPIView.as_view(), name='detail'),
     path('api/student/<int:pk>/update/', StudentUpdateAPIView.as_view(), name='update'),
     path('api/student/<int:pk>/delete/', StudentDeleteAPIView.as_view(), name='delete'),
+
+    path('api2/students/', StudentListAPIView2.as_view(), name='students-api2'),
+    path('api2/student/<int:pk>/', StudentAPIView2.as_view(), name='student-detail-api2'),
 ]
--- student/views.py
+++ student/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views import View
-from .models import Student
+from .models import ( Student )
 from django.views.generic import (
     ListView,
     DetailView,
@@ -22,6 +22,12 @@ from .serializers import (
     StudentDetailSerializers,
     StudentCreateSerializers,
 )
+
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 class StudentCreateAPIView(CreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentDetailSerializers
@@ -47,7 +53,6 @@ def home(request):
         'allstudents': Student.objects.all()
     }
     return render(request, 'home.html', context)
-
 
 class StudentListView(ListView):
     model = Student
@@ -76,3 +81,42 @@ class StudentDeleteView(DeleteView):
 
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
+
+
+class StudentListAPIView2(APIView):
+    def get(self, request, format=None):
+        students = Student.objects.all()
+        serializer = StudentDetailSerializers(students, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = StudentDetailSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StudentAPIView2(APIView):
+    def get_object(self, pk):
+        try:
+            return Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentDetailSerializers(student)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        student = self.get_object(pk)
+        serializer = StudentDetailSerializers(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        student = self.get_object(pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
